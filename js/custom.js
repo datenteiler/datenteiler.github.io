375d882c19dca4e9e490913c8469f8fa7989b24e
Christian Imhorst <christian.imhorst@gmail.com>
2017-06-09 20:46:03 +0200

    New files

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+'use strict';
+(function () {
+function copyright() {
+		var jetzt = new Date(),
+			jahr = jetzt.getFullYear(),
+            text = '&copy; PowerShell Usergroup Hannover ' + jahr;
+		document.getElementById('copyright').innerHTML = text;
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		copyright();
+	});
+}());
---
+++ js/meeting.js
@@ -0,0 +1,6 @@
+var text = {
+	"Datum": "16.06.2017", 
+	"Uhrzeit": "18:30", 
+	"Raum": "Netzweise",
+    "Ort": "Hannover"
+	};
