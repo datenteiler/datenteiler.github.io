032af766bc701b7d334918cba7c50797980b317c
Christian Imhorst <christian.imhorst@gmail.com>
2017-06-09 22:05:20 +0200

    change

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ function copyright() {
 
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true); // Replace 'meeting' with the path to your file
+    xobj.open('GET', 'data.json', true); // Replace 'data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
