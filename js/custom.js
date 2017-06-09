b32ad4e7f4d352a465851ca56a0481cae730cbae
Christian Imhorst <christian.imhorst@gmail.com>
2017-06-09 21:10:28 +0200

    changes

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,24 @@ function copyright() {
             text = '&copy; PowerShell Usergroup Hannover ' + jahr;
 		document.getElementById('copyright').innerHTML = text;
 	}
+ function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'meeting.json', true); // Replace 'meeting' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
 	document.addEventListener("DOMContentLoaded", function () {
 		copyright();
+		loadJSON(function(response) {
+			var Treffen = JSON.parse(response);
+			document.getElementById("meeting").innerHTML = 'Das nächste Treffen findet am ' + Treffen.Datum; // + ' um ' + text.Uhrzeit + ' in den Räumen von ' + text.Raum + ' in ' + text.Ort + ' statt.';
+		});
 	});
 }());
