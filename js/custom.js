ea85e568a2ef129cbbf8e8fa0013db71db767b3e
Christian Imhorst <christian.imhorst@gmail.com>
2017-06-11 20:00:00 +0200

    next try

--- js/custom.js
+++ js/custom.js
@@ -27,10 +27,6 @@ function copyright() {
 			' um ' + data.meeting.time + ' in den Räumen von ' + data.meeting.room + '</strong> ' + data.meeting.street + 
 			', ' + data.meeting.place + ' statt.';
 
-			var url = 'http://maps.google.com/maps?f=q&source=s_q&hl=de&geocode=&q=' +
-			          data.meeting.street + ',+' + data.meeting.place + ',+Bundesrepublik+Deutschland';
-			document.getElementById("route").innerHTML = '<a class="btn btn-default" href=' + url + ' role="button">Route planen &raquo;</a>';
-
 			document.getElementById("topics").innerHTML = '<strong>Vortrag 1:</strong> ' + data.topics.first + '<br />' +
 			'<strong>Vortrag 2:</strong> ' + data.topics.second + '<br />' +
 			'<strong>Außerdem:</strong> ' + data.topics.third;
